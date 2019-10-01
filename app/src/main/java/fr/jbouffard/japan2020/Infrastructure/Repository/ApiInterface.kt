0b69a0cd850277ef3f5c7ce4f2c90992e791f57a
julien.bouffard <julien.bouffard@link-value.fr>
2019-10-01 10:28:23 +0200

    mock api

--- app/src/main/java/fr/jbouffard/japan2020/Infrastructure/Repository/ApiInterface.kt
+++ app/src/main/java/fr/jbouffard/japan2020/Infrastructure/Repository/ApiInterface.kt
@@ -12,20 +12,20 @@ import retrofit2.http.POST
 interface ApiInterface {
 
     companion object {
-        const val BASE_URL = "https://fierce-oasis-90265.herokuapp.com"
-        //const val BASE_URL = "https://www.mocky.io/"
+        //const val BASE_URL = "https://fierce-oasis-90265.herokuapp.com"
+        const val BASE_URL = "https://www.mocky.io/"
     }
 
-    @POST("flights/offers")
-    //@POST("v2/5c4f3b983300002d00c583d9") //short stay
+    //@POST("flights/offers")
+    @POST("v2/5c4f3b983300002d00c583d9") //short stay
     //@POST("v2/5c2c82bb2e0000070ae875c0")
     fun getFlightOffersAsync(@Body flightRequest: FlightRequest): Deferred<List<FlightOffer>>
 
-    @POST("overnight/offers")
-    //@POST("v2/5bf289be2f00009700cfa128")
+    //@POST("overnight/offers")
+    @POST("v2/5bf289be2f00009700cfa128")
     fun getOvernightOffersAsync(@Body overnightRequest: OvernightRequest): Deferred<List<OvernightOffer>>
 
-    @GET("visits")
-    //@GET("v2/5bf289f02f00009700cfa12b")
+    //@GET("visits")
+    @GET("v2/5bf289f02f00009700cfa12b")
     fun getVisitsInfoAsync(): Deferred<List<Visit>>
 }
